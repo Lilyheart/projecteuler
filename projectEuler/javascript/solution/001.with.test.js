d9224ecfa07aebf213e1bc28e5fd96f640904b80
Lily Romano <Lilyheart@users.noreply.github.com>
2021-04-01 11:14:18 -0400

    Combine code and tests from 001-003.

--- projectEuler/javascript/solution/001.with.test.js
+++ projectEuler/javascript/solution/001.with.test.js
@@ -1,7 +1,14 @@
-/* global require, test, expect, multiplesOf3and5 */
+/* global module, test, expect */
 /* eslint-disable no-magic-numbers*/
 
-const multiplesOf3and5 = require("../solution/001");
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+function multiplesOf3and5(number) {
+  return Array.from(Array(number).keys())
+    .filter(function (val) {return val % 3 === 0 || val % 5 === 0;})
+    .reduce(function (acc, val) {return acc + val;});
+}
 
 test("multiplesOf3and5(49) should return 543", () => {
   expect(multiplesOf3and5(49)).toBe(543);
--- projectEuler/javascript/solution/003.with.test.js
+++ projectEuler/javascript/solution/003.with.test.js
@@ -1,4 +1,4 @@
-/* global module */
+/* global module, test, expect */
 /* eslint-disable no-magic-numbers*/
 // The prime factors of 13195 are 5, 7, 13 and 29.
 
@@ -40,4 +40,30 @@ function largestPrimeFactor(number) {
   return maxPrime;
 }
 
-module.exports = largestPrimeFactor;
+test("1 largestPrimeFactor(2) should return 2.", () => {
+  expect(largestPrimeFactor(2)).toBe(2);
+});
+
+test("2 largestPrimeFactor(3) should return 3.", () => {
+  expect(largestPrimeFactor(3)).toBe(3);
+});
+
+test("3 largestPrimeFactor(5) should return 5.", () => {
+  expect(largestPrimeFactor(5)).toBe(5);
+});
+
+test("4 largestPrimeFactor(7) should return 7.", () => {
+  expect(largestPrimeFactor(7)).toBe(7);
+});
+
+test("5 largestPrimeFactor(8) should return 2.", () => {
+  expect(largestPrimeFactor(8)).toBe(2);
+});
+
+test("6 largestPrimeFactor(13195) should return 29.", () => {
+  expect(largestPrimeFactor(13195)).toBe(29);
+});
+
+test("7 largestPrimeFactor(600851475143) should return 6857.", () => {
+  expect(largestPrimeFactor(600851475143)).toBe(6857);
+});
